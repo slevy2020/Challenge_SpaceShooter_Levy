ae7de65d4abab29f9bd736399d9aa537acffc4d6
slevy2020 <slevy@chadwickschool.org>
2018-12-20 09:11:01 -0800

    feat: commit all up thru lesson 1.6
    Thought I initialized earlier, but I guess I didn't:
    - add ship & movement
    - add background
    - add bolt
    - add lighting

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Boundary {
+  public float xMin, xMax, zMin, zMax;
+}
+
+public class PlayerController : MonoBehaviour
+{
+  public float speed;
+  public float Htilt;
+  public float Vtilt;
+  public Boundary boundary;
+
+  void FixedUpdate() {
+    float moveHorizontal = Input.GetAxis("Horizontal");
+    float moveVertical = Input.GetAxis("Vertical");
+
+    Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+    GetComponent<Rigidbody>().velocity = movement * speed;
+
+    GetComponent<Rigidbody>().position = new Vector3
+    (
+      Mathf.Clamp (GetComponent<Rigidbody>().position.x, boundary.xMin, boundary.xMax),
+      0.0f,
+      Mathf.Clamp (GetComponent<Rigidbody>().position.z, boundary.zMin, boundary.zMax)
+    );
+
+    GetComponent<Rigidbody>().rotation = Quaternion.Euler (GetComponent<Rigidbody>().velocity.z * Vtilt, 0.0f, GetComponent<Rigidbody>().velocity.x * -Htilt);
+  }
+}
